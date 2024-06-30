4e612554a610a29d4c8c2d6b5c3dd20b3d21a5ab
Lu Wan <wanlufun@gmail.com>
2024-06-30 14:29:46 +0800

    feat: add lazy.nvim

---
+++ init.lua
@@ -0,0 +1,30 @@
+-- Git >= 2.19.0 (for partial clones support)
+local git_version = vim.fn.system("git --version")
+local version_match = git_version:match("^git version ([^ ]+)")
+if version_match then
+  local major, minor, patch = version_match:match("(%d?)(%d?)(%d?)")
+  major = tonumber(major)
+  minor = tonumber(minor)
+  patch = tonumber(patch)
+  if major < 2 and minor < 19 and patch < 0 then
+    vim.api.nvim_echo({
+      -- lazy.nvim requires git >=2.19.0.
+      { "lazy.nvim requires Git >= 2.19.0\n", "ErrorMsg" },
+      { "Press any key to exit", "MoreMsg" },
+    }, true, {})
+    vim.fn.getchar()
+    vim.cmd([[quit]])
+    return {}
+  end
+else
+  vim.api.nvim_echo({
+    { "Failed to get Git version\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
+end
+
+require("wanlu.config.lazy")
+
---
+++ lua/wanlu/config/lazy.lua
@@ -0,0 +1,39 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { import = "wanlu/plugin" },
+  },
+  git = {
+    url_format = "https://gh-proxy.wanlu.fun/github.com/%s.git",
+    filter = true,
+  },
+  checker = {
+    enabled = true,
+  },
+  defaults = {
+    lazy = true,
+    version = false
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+	"tarPlugin",
+	"tohtml",
+	"tutor",
+	"zipPlugin",
+      },
+    },
+  },
+})
