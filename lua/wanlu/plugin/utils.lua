890c14598ba1dbe35cd0a8224d6df0ecaebd7b67
Lu Wan <wanlufun@gmail.com>
2024-07-01 15:25:00 +0800

    feat: add session management

--- lua/wanlu/plugin/utils.lua
+++ lua/wanlu/plugin/utils.lua
@@ -6,11 +6,29 @@ return {
       vim.opt.timeout = true
       vim.opt.timeoutlen = 300
     end,
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["<leader>q"] = { name = "+quit/session" },
+      },
+    },
     config = function(_, opts)
       local wk = require("which-key")
       wk.setup(opts)
       wk.register(opts.defaults)
     end,
+  },
+
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = vim.opt.sessionoptions:get() },
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
   },
 
   {
