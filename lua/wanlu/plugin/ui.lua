e54364d1c5da14b4c9ec0c3504976e9581378d73
Lu Wan <wanlufun@gmail.com>
2024-07-01 16:02:51 +0800

    feat: add start screen

--- lua/wanlu/plugin/ui.lua
+++ lua/wanlu/plugin/ui.lua
@@ -72,6 +72,63 @@ return {
   },
 
   {
+    "nvimdev/dashboard-nvim",
+    lazy = false,
+    opts = function()
+      local logo = [[
+██╗    ██╗ █████╗ ███╗   ██╗██╗     ██╗   ██╗
+██║    ██║██╔══██╗████╗  ██║██║     ██║   ██║
+██║ █╗ ██║███████║██╔██╗ ██║██║     ██║   ██║
+██║███╗██║██╔══██║██║╚██╗██║██║     ██║   ██║
+╚███╔███╔╝██║  ██║██║ ╚████║███████╗╚██████╔╝
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+          center = {
+            { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+            { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+            { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      if vim.o.filetype == "lazy" then
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+            end)
+          end,
+        })
+      end
+
+      return opts
+    end,
+  },
+
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
