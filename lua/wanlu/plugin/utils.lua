f9f8b58215104fbbdbd57dc0f0b8845ce6fac3df
Lu Wan <wanlufun@gmail.com>
2024-07-01 09:08:52 +0800

    feat: add file browse

---
+++ lua/wanlu/plugin/utils.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = true,
+  },
+
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+
+  {
+    "MunifTanjim/nui.nvim",
+    lazy = true,
+  }
+}
+
