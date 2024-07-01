edd2cdefb4f427b0ab4aaf7138ee495dc81e8502
Lu Wan <wanlufun@gmail.com>
2024-07-01 16:58:56 +0800

    feat: add LazyFile support

--- lua/wanlu/plugin/init.lua
+++ lua/wanlu/plugin/init.lua
@@ -1,6 +1,8 @@
 -- Set global utils var
 _G.LuUtil = require("wanlu.util")
 
+LuUtil.plugin.setup()
+
 return {
   { "folke/lazy.nvim", version = "*" },
 }
---
+++ lua/wanlu/util/plugin.lua
@@ -0,0 +1,35 @@
+M = {}
+
+M.lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
+
+function M.setup()
+  M.lazy_file()
+end
+
+function M.lazy_file()
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    once = true,
+    callback = function(event)
+      if vim.v.vim_did_enter == 1 then
+        return
+      end
+
+      local ft = vim.filetype.match({ buf = event.buf })
+      if ft then
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
+          vim.bo[event.buf].syntax = ft
+        end
+
+        vim.cmd([[redraw]])
+      end
+    end,
+  })
+
+  local Event = require("lazy.core.handler.event")
+
+  Event.mappings.LazyFile = { id = "LazyFile", event = M.lazy_file_events }
+  Event.mappings["User LazyFile"] = Event.mappings.LazyFile
+end
+
+return M
