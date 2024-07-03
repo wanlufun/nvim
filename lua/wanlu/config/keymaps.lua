0f000f15c8b177180aea5b26d3fd3adc1ac257a6
Lu Wan <wanlufun@gmail.com>
2024-07-03 23:09:36 +0800

    refactor: Extract some functions and encapsulate into the util package

--- lua/wanlu/config/keymaps.lua
+++ lua/wanlu/config/keymaps.lua
@@ -1,22 +1,4 @@
-function safe_keymap_set(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  local modes = type(mode) == "string" and { mode } or mode
-
-  modes = vim.tbl_filter(function(m)
-    return not (keys.have and keys:have(lhs, m))
-  end, modes)
-
-  if #modes > 0 then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap and not vim.g.vscode then
-      opts.remap = nil
-    end
-    vim.keymap.set(modes, lhs, rhs, opts)
-  end
-end
-
-local map = safe_keymap_set
+local map = LuUtil.safe_keymap_set
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
--- lua/wanlu/util/init.lua
+++ lua/wanlu/util/init.lua
@@ -38,6 +38,24 @@ function M.on_very_lazy(fn)
   })
 end
 
+function M.safe_keymap_set(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  local modes = type(mode) == "string" and { mode } or mode
+
+  modes = vim.tbl_filter(function(m)
+    return not (keys.have and keys:have(lhs, m))
+  end, modes)
+
+  if #modes > 0 then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(modes, lhs, rhs, opts)
+  end
+end
+
 function M.is_loaded(name)
   local Config = require("lazy.core.config")
   return Config.plugins[name] and Config.plugins[name]._.loaded
