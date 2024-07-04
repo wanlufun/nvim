99c650e210e7a828ca45aa7c83670c8d503df9bc
Lu Wan <wanlufun@gmail.com>
2024-07-04 14:48:19 +0800

    feat: disabled buffer close icon

--- lua/wanlu/plugin/ui.lua
+++ lua/wanlu/plugin/ui.lua
@@ -156,6 +156,7 @@ return {
         },
         separator_style = "slant",
         show_close_icon = false,
+        show_buffer_close_icons = false,
         always_show_bufferline = false,
       },
     },
