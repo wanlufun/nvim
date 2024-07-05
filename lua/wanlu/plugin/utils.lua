fde6f8fe858fa7ae7b6f946335bfaff27e85f763
Lu Wan <wanlufun@gmail.com>
2024-07-05 12:41:55 +0800

    feat(ui): use `mini.icons` instand of `nvim-web-devicons`

--- lua/wanlu/plugin/utils.lua
+++ lua/wanlu/plugin/utils.lua
@@ -44,8 +44,15 @@ return {
   },
 
   {
-    "nvim-tree/nvim-web-devicons",
+    "echasnovski/mini.icons",
     lazy = true,
+    opts = {},
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
   },
 
   {
